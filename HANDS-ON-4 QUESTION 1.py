d5ddd39829fc6be5e882463e9273851eedac752e
SashankTsavalam21 <sashanktsavalam@gmail.com>
2024-09-12 11:42:23 -0500

    Update HANDS-ON-4 QUESTION 1.py

--- HANDS-ON-4 QUESTION 1.py
+++ HANDS-ON-4 QUESTION 1.py
@@ -10,3 +10,11 @@ n = int(input("Enter any value to calculate Fibonacci sequence(n): "))
 
 result = fib(n)
 print(f"Fibonacci({n}): {result}")
+
+
+
+
+''' fib(5) -> fib(4) -> fib(3) -> fib(2) -> fib(1) -> fib(0)
+Return to fib(2) and fib(1), then return to fib(3).
+fib(5) -> fib(4) calls fib(2) -> fib(1) -> fib(0).
+fib(5) calls fib(3) -> fib(2) -> fib(1) -> fib(0) '''
